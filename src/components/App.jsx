8e76401f7c3a828d0785a3e94001f3115cc54c5f
ZVA-23 <zva-23@ukr.net>
2022-10-04 18:12:48 +0300

    add profile component and style

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
+import user from '../data/user.json';
+import { Profile } from './Profile/Profile';
+
+// import data from '../data/data.json';
+// import { StaticsList } from './StatisticsList/StatisticsList';
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+
+      {/* <StaticsList title="Upload stats" stats={data} /> */}
+      
+    </>
+
   );
 };
+
---
+++ src/components/StatisticsList/StatisticsItem.jsx
@@ -0,0 +1,15 @@
+// import { PropTypes } from "prop-types";
+
+// export const StatisticsItem = ({ label, percentage }) => {
+//     return (
+//         <>
+//             <span class="labeldoc">{label}</span>
+//             <span class="percentage">{percentage}%</span>
+//         </>
+//     )
+// }
+
+// StatisticsItem.propTypes - {
+//     label: PropTypes.string.isRequired,
+//     percentage: PropTypes.number.isRequired,
+// }
